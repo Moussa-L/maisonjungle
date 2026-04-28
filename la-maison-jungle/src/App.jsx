a6f53a059b024ae360f68d3427785efe5a8e31be
Moussa lidya <moussalidya07@gmail.com>
2026-04-28 16:35:38 +0300

    React

---
+++ la-maison-jungle/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+//ici on importe le composant Banner que nous allons créer dans le dossier components
+import Banner from './components/Banner'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Banner />    
+    </>
+  )
+}
+
+export default App
---
+++ la-maison-jungle/src/components/Banner.jsx
@@ -0,0 +1,12 @@
+/*
+ Banner.jsx est le composant qui affiche la bannière de la page d'accueil de l'application(la barre de navigation.)
+*/
+
+
+const Banner = () => {
+    return <h1 >Bienvenue sur à la maison jungle</h1>
+    
+}
+ 
+//exportation du composant Banner pour pouvoir l'utiliser dans d'autres fichiers
+export default Banner;
